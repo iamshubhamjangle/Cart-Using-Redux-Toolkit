28f75a896fff454300547f9de3ff3db7601a8be8
Shubham Jangle <jangleshubham13@gmail.com>
2022-06-14 13:03:21 +0530

    added fun to cal total when change in cartitem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Navbar from "components/Navbar";
 import CartContainer from "components/CartContainer";
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
